aa9d5612ec9a12f62fb9b4bad5479e46661fdd03
MrDoritos <ianmoore322@gmail.com>
2024-08-31 10:46:30 -0700

    Refactor

--- macro-calculator.cpp
+++ macro-calculator.cpp
@@ -5,71 +5,73 @@
 #include <string.h>
 
 int main() {
-double targetCalories = 0.0d;
-do {
-printf("Target kilocalorie count: ");
-} while (scanf("%lf", &targetCalories) != 1);
+    double targetCalories = 0.0d;
 
-double targetProtein = 0.0d;
-do {
-printf("Target protein amount (your body weight in pounds): ");
-} while (scanf("%lf", &targetProtein) != 1);
+    do {
+        printf("Target kilocalorie count: ");
+    } while (scanf("%lf", &targetCalories) != 1);
 
-double proteinCalories = targetProtein * 4;
+    double targetProtein = 0.0d;
 
-double otherCalories = targetCalories - proteinCalories;
+    do {
+        printf("Target protein amount (your body weight in pounds): ");
+    } while (scanf("%lf", &targetProtein) != 1);
 
-double carbohydrates = (otherCalories / 2) / 4;
+    double proteinCalories = targetProtein * 4;
 
-double fats = (otherCalories / 2) / 9;
+    double otherCalories = targetCalories - proteinCalories;
 
-int width = CONSOLEWIDTH;
+    double carbohydrates = (otherCalories / 2) / 4;
 
-char* row = (char*)alloca(sizeof(char) * width + 1);
-char* rowBuf = (char*)alloca(sizeof(char) * width + 1);
+    double fats = (otherCalories / 2) / 9;
 
-memset(rowBuf, '\0', width + 1);
-memset(rowBuf, ' ', width);
-memset(row, '-', width);
-row[width] = '\0';
-row[width - 1] = '+';
-row[22 < width ? 22 : 0] = '+';
-row[0] = '+';
+    int width = CONSOLEWIDTH;
 
-puts(row);
+    char* row = new char[width + 1];
+    char* rowBuf = new char[width + 1];
 
-sprintf(rowBuf, "  Your calorie target | %lf kcal", targetCalories);
-rowBuf[0] = '|';
-rowBuf[width - 1] = '|';
+    memset(rowBuf, '\0', width + 1);
+    memset(rowBuf, ' ', width);
+    memset(row, '-', width);
 
-puts(rowBuf);
+    row[width] = '\0';
+    row[width - 1] = '+';
+    row[22 < width ? 22 : 0] = '+';
+    row[0] = '+';
 
-memset(rowBuf, ' ', width);
-rowBuf[width] = 0;
-sprintf(rowBuf, "  Your protein target | %lf grams, %lf kcal, %lf%%", targetProtein, proteinCalories, (proteinCalories / targetCalories) * 100);
-rowBuf[0] = '|';
-rowBuf[width - 1] = '|';
+    puts(row);
 
-puts(rowBuf);
+    sprintf(rowBuf, "  Your calorie target | %lf kcal", targetCalories);
+    rowBuf[0] = '|';
+    rowBuf[width - 1] = '|';
 
-memset(rowBuf, ' ', width);
-rowBuf[width] = 0;
-sprintf(rowBuf, "  Your carb target    | %lf grams, %lf kcal, %lf%%", carbohydrates, (otherCalories / 2), ((otherCalories / 2) / targetCalories) * 100);
-rowBuf[0] = '|';
-rowBuf[width - 1] = '|';
+    puts(rowBuf);
 
-puts(rowBuf);
+    memset(rowBuf, ' ', width);
+    rowBuf[width] = 0;
+    sprintf(rowBuf, "  Your protein target | %lf grams, %lf kcal, %lf%%", targetProtein, proteinCalories, (proteinCalories / targetCalories) * 100);
+    rowBuf[0] = '|';
+    rowBuf[width - 1] = '|';
 
-memset(rowBuf, ' ', width);
-rowBuf[width] = 0;
-sprintf(rowBuf, "  Your fat target     | %lf grams, %lf kcal, %lf%%", fats, (otherCalories / 2), ((otherCalories / 2) / targetCalories) * 100);
-rowBuf[0] = '|';
-rowBuf[width - 1] = '|';
+    puts(rowBuf);
 
-puts(rowBuf);
+    memset(rowBuf, ' ', width);
+    rowBuf[width] = 0;
+    sprintf(rowBuf, "  Your carb target    | %lf grams, %lf kcal, %lf%%", carbohydrates, (otherCalories / 2), ((otherCalories / 2) / targetCalories) * 100);
+    rowBuf[0] = '|';
+    rowBuf[width - 1] = '|';
 
-puts(row);
+    puts(rowBuf);
 
-puts("The FDA recommends 300 grams of carbohydrates and 65 grams of fat.");
+    memset(rowBuf, ' ', width);
+    rowBuf[width] = 0;
+    sprintf(rowBuf, "  Your fat target     | %lf grams, %lf kcal, %lf%%", fats, (otherCalories / 2), ((otherCalories / 2) / targetCalories) * 100);
+    rowBuf[0] = '|';
+    rowBuf[width - 1] = '|';
 
+    puts(rowBuf);
+
+    puts(row);
+
+    puts("The FDA recommends 300 grams of carbohydrates and 65 grams of fat.");
 }
